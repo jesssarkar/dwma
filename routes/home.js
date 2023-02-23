d2b560819d2fcc9db66e6766fb33fe902eb1f59d
Jessica Sarkar <jessicalosee@gmail.com>
2023-02-22 21:14:58 -0600

    added home routes and controllers

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const homeController = require()
+const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
 router.post('/new', homeController.createItem)
