67df6ffd97e64fd1458463e93705068bbb383e8a
Jessica Sarkar <jessicalosee@gmail.com>
2023-03-23 18:32:32 -0500

    added update and delete functionality

--- controllers/edit.js
+++ controllers/edit.js
@@ -12,10 +12,30 @@ module.exports = {
     },
 
     deleteItem: async (req, res) => {
+        const id = req.params.id
+        try {
+            await ItemList.findByIdAndDelete(id)
+            res.redirect('back')
+        } catch (err){
+            return res.status(500).send(err)
+        } 
 
     },
 
     updateItem: async (req, res) => {
+        const id = req.params.id
+        try {
+            await ItemList.findByIdAndUpdate(id,
+                {
+                    textinput: req.body.textinput,
+                    numinput: req.body.numinput  
+                },
+            )
+            res.redirect('/');
+        } catch (err){
+            if (err) return res.status(500).send(err)
+            res.redirect('/');
+        } 
 
     },
 }
